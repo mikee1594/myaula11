964acf1dcfa67fb7a4d932a6b9dff2eb2f310524
mikee1594 <mikeematos1594@gmail.com>
2018-05-10 12:16:25 +0100

    usar lista

--- LotsOfStuff/Bag.cs
+++ LotsOfStuff/Bag.cs
@@ -6,15 +6,10 @@ namespace Aula11
     /// <summary>
     /// Classe que representa uma mochila ou saco que contem itens
     /// </summary>
-    public class Bag : IStuff
+    public class Bag : List <IStuff>, IStuff
     {
-        /// <summary>Array que contém os itens da mochila</summary>
-        private List<IStuff> stuff;
-
-        /// <summary>Número de itens na mochila</summary>
-        public int StuffCount { get { return stuff.Count;  } }
-
-        /// <summary> 
+       
+         /// <summary> 
         /// Propriedade Weight respeita o contrato com IHasWeight. No caso do
         /// Bag o peso vai corresponder ao peso total dos itens.
         /// </summary>
@@ -23,12 +18,11 @@ namespace Aula11
             get
             {
                 float totalWeight = 0;
-                foreach (IStuff aThing in stuff)
+                foreach (IStuff aThing in this)
                 {
-                    if (aThing != null)
-                    {
-                        totalWeight += aThing.Weight;
-                    }
+                    
+                   totalWeight += aThing.Weight;
+                
                 }
                 return totalWeight;
             }
@@ -43,12 +37,12 @@ namespace Aula11
             get
             {
                 float totalValue = 0;
-                foreach (IStuff aThing in stuff)
+                foreach (IStuff aThing in this)
                 {
-                    if (aThing != null)
-                    {
-                        totalValue += aThing.Value;
-                    }
+                
+                
+                  totalValue += aThing.Value;
+                
                 }
                 return totalValue;
             }
@@ -60,37 +54,12 @@ namespace Aula11
         /// <param name="bagSize">
         /// Número máximo de itens que é possível colocar na mochila
         /// </param>
-        public Bag(int bagSize)
+        public Bag(int bagSize) : base(bagSize)
         {
-            stuff = new  List<IStuff> (bagSize);
+         
            
         }
 
-        /// <summary>Colocar um item na mochila</summary>
-        /// <param name="aThing">Item a colocar na mochila</param>
-        public void AddThing(IStuff aThing)
-        {
-           
-
-            // Adicionar o item à mochila e depois incrementar o
-            // número de coisas na mochila
-           
-            stuff.Add(aThing);
-        }
-
-        /// <summary>Observar um item da mochila sem o remover da mesma</summary>
-        /// <param name="index">Local onde está o item a observar</param>
-        /// <returns>Item a ser observado</returns>
-        public IStuff GetThing(int index)
-        {
-            if (index > stuff.Count)
-            {
-                // Senão existir um item no local indicado, "lançar" uma exceção
-                throw new InvalidOperationException(
-                    "Bag doesn't have that much stuff!");
-            }
-            return stuff[index]; // --> remover o iStuff
-        }
 
         /// <summary>
         /// Sobreposição do método ToString() para a classe Bag.
@@ -100,7 +69,7 @@ namespace Aula11
         /// </returns>
         public override string ToString()
         {
-            return $"Mochila com {StuffCount} itens e um peso e valor " +
+            return $"Mochila com {Count} itens e um peso e valor " +
                 $"totais de {Weight} Kg e {Value} EUR, respetivamente";
         }
     }
